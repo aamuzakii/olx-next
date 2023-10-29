a1ad00bad4c96c0ab96012ba8d6636f1f6b0ae8f
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-30 06:30:01 +0700

    del

--- server/models/index.ts
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-
-const sequelize = new Sequelize('database', 'username', 'password', {
-  host: 'localhost',
-  database: 'olx',
-  dialect: 'postgres',
-  username: 'postgres',
-  password: '123'
-})
