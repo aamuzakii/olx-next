e420051fe2e14827d96ff5f916388ebdf619bf61
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-29 20:53:37 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,14 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div></div>
+      <div className={styles.card}>
+        <Image
+          src="https://apollo-singapore.akamaized.net/v1/files/akka05nkztsf2-ID/image"
+          alt=""
+          width={215}
+          height={153}
+        ></Image>
+      </div>
     </main>
   );
 }
