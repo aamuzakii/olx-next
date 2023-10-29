f235bd4a34d6d7dcc26feed9002f6859f65e1d30
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-30 06:08:49 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -4,13 +4,17 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <main className={styles.main}>
-      <div className={styles.card}>
-        <Image
-          src="https://apollo-singapore.akamaized.net/v1/files/akka05nkztsf2-ID/image"
-          alt=""
-          width={215}
-          height={153}
-        ></Image>
+      <div className={styles.grid}>
+        {[1, 1, 1, 1, 1, 1].map((x) => (
+          <div className={styles.kartu}>
+            <Image
+              src="https://apollo-singapore.akamaized.net/v1/files/akka05nkztsf2-ID/image"
+              alt=""
+              width={215}
+              height={153}
+            ></Image>
+          </div>
+        ))}
       </div>
     </main>
   );
