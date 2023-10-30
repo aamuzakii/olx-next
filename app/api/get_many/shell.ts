42be6dc0e80c8d5a608fd915ec2194bc055cfa87
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-30 09:10:46 +0700

    ok

--- app/api/get_many/shell.ts
+++ app/api/get_many/shell.ts
@@ -1,6 +1,7 @@
 const util = require("util");
 const exec = util.promisify(require("child_process").exec);
 import fs from "fs";
+
 const command =
   'curl -o list.txt "https://www.olx.co.id/depok-kota_g4000024/disewakan-rumah-apartemen_c5160?filter=price_between_1700000_to_2000000,type_eq_rumah"';
 
@@ -9,10 +10,6 @@ export async function getManyHouses() {
     const { stdout, stderr } = await exec(command);
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
-
-    // await new Promise(() => {
-    //   setTimeout(() => {}, 2000);
-    // });
 
     fs.readFile("list.txt", "utf8", (err, data) => {
       if (err) {
@@ -32,12 +29,46 @@ export async function getManyHouses() {
           if (match) {
             const hrefContent = match[1];
             console.log(hrefContent);
+            const fullLink = `https://www.olx.co.id${hrefContent}`;
+            getHouseDetail(fullLink);
           } else {
             console.log("href attribute not found");
           }
         }
       } else {
         console.log("No matching tags found in the HTML content.");
+      }
+    });
+  } catch (error: any) {
+    console.error(`error: ${error.message}`);
+  }
+}
+
+export async function getHouseDetail(link: string) {
+  const command = `curl -o detail.txt "${link}"`;
+
+  try {
+    const { stdout, stderr } = await exec(command);
+    console.log(`stdout: ${stdout}`);
+    console.log(`stderr: ${stderr}`);
+
+    const fileName = `${link}.txt`;
+
+    fs.readFile(fileName, "utf8", (err, data) => {
+      if (err) {
+        console.error("Error reading the file:", err);
+        return;
+      }
+
+      var pattern = /apollo-singapore(.*?)"[^"]+/;
+
+      var match = data.match(pattern);
+
+      if (match) {
+        var extractedText = match[1];
+        console.log(extractedText);
+      } else {
+        console.log("No match found.");
       }
     });
   } catch (error: any) {
