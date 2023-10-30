c2460ef1da7020362a1d1d754b94c2a425b42f0d
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-30 20:39:14 +0700

    ok

--- app/api/get_many/shell.ts
+++ app/api/get_many/shell.ts
@@ -8,9 +8,6 @@ const command =
 
 export async function getManyHouses() {
   try {
-    // const { stdout, stderr } = await exec(command);
-    // console.error(`stderr: ${stderr}`);
-
     fs.readFile("list.txt", "utf8", (err, data) => {
       if (err) {
         console.error("Error reading the file:", err);
@@ -18,13 +15,9 @@ export async function getManyHouses() {
       }
 
       const { JSDOM } = require("jsdom");
-
       const dom = new JSDOM(data);
-
-      // Access the document
       const document = dom.window.document;
 
-      // Use querySelector to find the span with data-aut-id="itemPrice"
       const targetSpan = document.querySelector(
         'span[data-aut-id="itemPrice"]'
       );
@@ -32,22 +25,15 @@ export async function getManyHouses() {
       if (targetSpan) {
         const elementCollection =
           targetSpan.parentElement.parentElement.parentElement.parentElement;
-
         const loopable = elementCollection.children;
 
         for (let i = 0; i < loopable.length - 1; i++) {
           const element = loopable[i];
-
           const mma = element.children[0];
-
           const linkStr = mma.href;
-
           const image = mma.children[0];
-
           const imagebener = image.children[1];
-
           const htmlString = imagebener.innerHTML;
-
           const srcRegex = /src="([^"]+)"/;
           const match = htmlString.match(srcRegex);
 
@@ -61,13 +47,10 @@ export async function getManyHouses() {
           }
 
           const metadata = mma.children[1];
-
           const harga = metadata.children[0]?.textContent;
           const feature = metadata.children[1]?.textContent;
           const judul = metadata.children[2]?.textContent;
           const publishStr = metadata.children[3]?.children[1]?.textContent;
-
-          // console.log(harga, feature, judul, publishStr);
 
           const finalObj = {
             url: linkStr,
