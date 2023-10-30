1c409e506fb559d1247d9dde6e10ef25262478ff
Muzaki新联想 <aamuzakii@gmail.com>
2023-10-30 07:18:06 +0700

    ok

--- app/api/get_many/route.ts
+++ app/api/get_many/route.ts
@@ -1,25 +1,26 @@
 import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
-const { exec } = require("child_process");
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
 
 const command =
   'curl -o list.txt "https://www.olx.co.id/depok-kota_g4000024/disewakan-rumah-apartemen_c5160?filter=price_between_1700000_to_2000000,type_eq_rumah"';
 
 export async function GET() {
-  exec(command, (error: any, stdout: any, stderr: any) => {
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-    if (stderr) {
+  async function runCommand() {
+    try {
+      const { stdout, stderr } = await exec(command);
+      console.log(`stdout: ${stdout}`);
       console.log(`stderr: ${stderr}`);
-      return;
+    } catch (error: any) {
+      console.error(`error: ${error.message}`);
     }
-    console.log(`stdout: ${stdout}`);
-  });
+  }
+
+  runCommand();
+
   const prisma = new PrismaClient();
 
-  const user = await prisma.house.findFirst();
   await prisma.$disconnect();
 
   return NextResponse.json({ data: 123 });
