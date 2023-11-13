c9d4a239e241d525e5434d03c6a905ba244e531a
Muzaki新联想 <aamuzakii@gmail.com>
2023-11-14 06:09:39 +0700

    ok

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,36 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { exec } from "child_process";
+
+export async function GET() {
+  const prisma = new PrismaClient();
+
+  const houses = await prisma.house.findMany({
+    where: {
+      deleted: false,
+      price: {
+        lte: 2500000,
+      },
+    },
+  });
+
+  houses.forEach(async (house, i) => {
+    const response = await fetch(house.url);
+
+    if (!response.ok && response.status === 404) {
+      const deleted = await prisma.house.update({
+        where: {
+          id: house.id,
+        },
+        data: {
+          // deleted: true,
+          // comment: "404",
+        },
+      });
+      console.log("deleted:", deleted.title);
+    }
+  });
+  await prisma.$disconnect();
+
+  return NextResponse.json({ data: houses });
+}
---
+++ app/api/revert/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+import { exec } from "child_process";
+
+export async function GET() {
+  const prisma = new PrismaClient();
+
+  const houses = await prisma.house.updateMany({
+    where: {
+      comment: "404",
+    },
+    data: {
+      comment: "",
+    },
+  });
+
+  console.log(houses);
+
+  await prisma.$disconnect();
+
+  return NextResponse.json({ data: houses });
+}
