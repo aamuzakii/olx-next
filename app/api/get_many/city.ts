e153ce26db838b2cf80564e7f7c214d293cf1155
Muzaki新联想 <aamuzakii@gmail.com>
2023-11-15 06:40:39 +0700

    tangerangKota

--- app/api/get_many/city.ts
+++ app/api/get_many/city.ts
@@ -3,6 +3,7 @@ export const bogorKab = "bogor-kab_g4000004";
 export const bekasiKota = "bekasi-kota_g4000020";
 export const bogorKota = "bogor-kota_g4000021";
 export const tangsel = "tangerang-selatan-kota_g4000080";
+export const tangerangKota = "tangerang-kota_g4000079";
 
 export const cileungsi = "cileungsi_g5000804";
 export const gunungPutri = "gunung-putri_g5000810";
