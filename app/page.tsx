3a31019d5c6192745d8d9852a490b476e12a1819
Muzaki新联想 <aamuzakii@gmail.com>
2023-11-16 13:44:48 +0700

    ok

--- app/page.tsx
+++ app/page.tsx
@@ -69,6 +69,9 @@ export default function Home() {
       },
       body: JSON.stringify({ comment }),
     });
+    // const x = document.getElementsByTagName("form")[id];
+    // x.style.display = "none";
+    setEmail("");
     await checkUserLoggedIn();
   };
 
