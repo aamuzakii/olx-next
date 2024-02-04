374e721837fe3f3cbe9eca28670e5059674624cd
Muzaki新联想 <aamuzakii@gmail.com>
2024-02-04 19:17:58 +0700

    create job page

--- app/jobs/page.tsx
+++ app/jobs/page.tsx
@@ -1,7 +1,38 @@
+"use client";
 import React from "react";
+import style from "../page.module.css";
+import Card from "../card";
 
 const page = () => {
-  return <div>page</div>;
+  interface IFoo {
+    price: string;
+  }
+
+  return (
+    <>
+      <nav>
+        <button onClick={() => {}}>Refetch</button>
+        <button onClick={() => {}}>Revalidate</button>
+      </nav>
+      <section>
+        <ul>
+          <li>
+            Jarak max 23.3km ke Aruna ataupun Alodokter. Jangan lebih, karena
+            kalo WFO lu beneran tiap hari PP
+          </li>
+          <li>Banjir paling cek nya last ya</li>
+        </ul>
+      </section>
+      <main className={style.main}>
+        <div className={style.grid}>
+          {[].map((h, i) => {
+            let formattedNumber = "";
+            return <h1></h1>;
+          })}
+        </div>
+      </main>
+    </>
+  );
 };
 
 export default page;
