0727070b588467991d169bdb2560bbb1361c5a64
Muzaki新联想 <aamuzakii@gmail.com>
2024-05-05 14:12:58 +0700

    skip for vercel deployment

--- app/api/get_many/shell.ts
+++ app/api/get_many/shell.ts
@@ -6,6 +6,7 @@ import fs from "fs/promises";
 const { JSDOM } = require("jsdom");
 
 export async function getHousesByCity(city: string) {
+  if ((process.env.API_URL)?.includes('vercel')) return [] // skip for vercel deployment
   const sortByDate = "&sorting=desc-creation";
   const sortByRelevance = "&sorting=desc-relevance";
   const empty = "";
