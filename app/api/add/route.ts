b8f9828ccda8ffb76a7cfa38900aa0a9b6a7bfc5
Muzaki新联想 <aamuzakii@gmail.com>
2024-05-05 17:05:21 +0700

    remove add functionality

--- app/api/add/route.ts
+++ app/api/add/route.ts
@@ -3,14 +3,7 @@ import { NextResponse } from "next/server";
 import { PrismaClient } from "@prisma/client";
 
 export async function GET() {
-  const prisma = new PrismaClient();
 
-  const user = await prisma.house.create({
-    data: {
-      url: "ff",
-    },
-  });
-  await prisma.$disconnect();
 
-  return NextResponse.json({ data: user });
+  return NextResponse.json({ data: 1 });
 }
