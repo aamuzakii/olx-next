d4aa38f15542914c1107988fe95a38137e6e5c22
Muzaki新联想 <aamuzakii@gmail.com>
2024-05-10 20:05:06 +0700

    add query logger

--- app/api/jobs/route.ts
+++ app/api/jobs/route.ts
@@ -14,7 +14,32 @@ const where = {
 };
 
 export async function GET() {
-  const prisma = new PrismaClient();
+  const prisma = new PrismaClient({
+    log: [
+      {
+        emit: 'event',
+        level: 'query',
+      },
+      {
+        emit: 'stdout',
+        level: 'error',
+      },
+      {
+        emit: 'stdout',
+        level: 'info',
+      },
+      {
+        emit: 'stdout',
+        level: 'warn',
+      },
+    ],
+  })
+
+  prisma.$on('query', (e) => {
+    console.log('Query: ' + e.query)
+    console.log('Params: ' + e.params)
+    console.log('Duration: ' + e.duration + 'ms')
+  })
 
   const houses = await prisma.job.findMany({
     where,
