3bf656f8b250fd4ec45ad1fff52ea578aa266783
Muzaki新联想 <aamuzakii@gmail.com>
2024-05-11 08:59:42 +0700

    add RSC

--- app/jobs/page-successful-rsc.tsx
+++ app/jobs/page-successful-rsc.tsx
@@ -4,26 +4,51 @@ import Card from "../card";
 import Trash from "../atom/Trash";
 import Chevron from "../atom/Chevron";
 import { countries } from "./country";
+import { PrismaClient } from "@prisma/client";
 
 interface IJob {
   id: number;
   url: string;
   title: string;
-  stack: string;
-  country: string;
-  candidates: string;
-  description: string;
-  date: string;
-  fee: string;
+  stack: string | null;
+  country: string | null;
+  candidates: string | null;
+  description: string | null;
+  date: string | null;
+  fee: string | null;
 }
 
 const nextApiUrl = process.env.NEXT_API_URL || "";
 
 async function getJobDataNext13() {
-  const res = await fetch(`${nextApiUrl}/api/jobs`, { cache: "no-store" });
+  // const res = await fetch(`/api/jobs`, { cache: "no-store" });
 
-  const data = await res.json();
-  return data.data;
+  // const data = await res.json();
+
+  const api = await import("../api/jobs/route");
+  // const foo = await (await res.handler()).json();
+
+  const where = {
+    deleted: false || null,
+    candidates: {
+      in: ["Less than 5", "5 to 10", "10 to 15"],
+    },
+    country: {
+      notIn: ["India", "Israel", "United Kingdom", "United States"],
+    },
+  };
+
+  const prisma = new PrismaClient();
+
+  const houses = await prisma.job.findMany({
+    where,
+    orderBy: {
+      id: "asc",
+    },
+  });
+  await prisma.$disconnect();
+
+  return houses;
 }
 
 const page = async () => {
@@ -55,7 +80,7 @@ const page = async () => {
           {list.map((h) => {
             let formattedNumber = "";
 
-            const stacks = JSON.parse(h.stack);
+            const stacks = JSON.parse(h.stack || "");
             return (
               <div className={style.card} key={h.id}>
                 <p>{h.date}</p>
@@ -78,7 +103,7 @@ const page = async () => {
                     width: 40,
                   }}
                 >
-                  {countries[h.country]?.flag}
+                  {countries[h.country || ""]?.flag}
                 </div>
                 <p>Proposals: {h.candidates}</p>
                 <div className={style.interaction}>
